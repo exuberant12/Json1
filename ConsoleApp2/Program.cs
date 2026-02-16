771d952ecb64d0638220e201d0e7ee87204f9796
takacsk2 <takacsk@310-05>
2026-02-16 09:34:17 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    public class  adatok
+    {
+        public string nevek { get; set; }
+        public int korok { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String fajl = File.ReadAllText("adatok.json", System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl);
+
+        }
+    }
+}
